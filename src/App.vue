0aea977bfae55523fc8d3d87839c4baf7ad69feb
mfimahendra <mfimahendra@gmail.com>
2023-01-09 21:28:05 +0700

    first init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import NavBar from './components/NavBar.vue'
+
+</script>
+
+<template>
+  <div>
+    <NavBar />
+    <router-view />  
+  </div>        
+</template>
+
+<script>
+
+export default {
+  name: "App",
+};
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="home">
+    <h1>Tes</h1>
+  </div>
+</template>
+
+<script>
+
+</script>
