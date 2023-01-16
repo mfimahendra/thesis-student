d3d063911943dedaf4e53d133f82517fad120cba
mfimahendra <mfimahendra@gmail.com>
2023-01-16 23:11:03 +0700

    dashboard update

--- src/views/admin/DashboardView.vue
+++ src/views/admin/DashboardView.vue
@@ -4,8 +4,82 @@ import Sidebar from '../../components/SideBarAdmin.vue'
 </script>
 
 <template>
-<Navbar />
-<Sidebar />
+    <Navbar />
+    <Sidebar />
+    <div class="content-wrapper">
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">Dashboard</h1>
+                    </div>
+                    <div class="col-sm-6">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <section class="content">
+            <div class="row">
+                <div class="col-lg-3 col-6">
+
+                    <div class="small-box bg-info">
+                        <div class="inner">
+                            <h3>150</h3>
+                            <p>Cities</p>
+                        </div>
+                        <div class="icon">
+                            <i class="nav-icon fas fa-city" style="font-size:60px;"></i>
+                        </div>
+                        <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-6">
+
+                    <div class="small-box bg-success">
+                        <div class="inner">
+                            <h3>130</h3>
+                            <p>Universities</p>
+                        </div>
+                        <div class="icon">
+                            <i class="nav-icon fas fa-university"></i>
+                        </div>
+                        <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-6">
+
+                    <div class="small-box bg-warning">
+                        <div class="inner">
+                            <h3>44</h3>
+                            <p>Alumni</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fas fa-user-graduate"></i>
+                        </div>
+                        <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-6">
+
+                    <div class="small-box bg-danger">
+                        <div class="inner">
+                            <h3>65</h3>
+                            <p>Students</p>
+                        </div>
+                        <div class="icon">
+                            <i class="nav-icon fas fa-user"></i>
+                        </div>
+                        <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+    </div>
 
 </template>
 
